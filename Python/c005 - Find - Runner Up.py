d8a7779a12eaf9ecd49d5c001818a199bcca5ba2
Patrick <P_W_Doyle@hotmail.com>
2019-07-20 07:06:24 -0500

    Finished 5

--- Python/c005 - Find - Runner Up.py
+++ Python/c005 - Find - Runner Up.py
@@ -4,6 +4,15 @@
 
 if __name__ == '__main__':
     n = 5#int(input())
-    arr = map(int, [2, 3, 6, 6, 5].split()) #map(int, input().split())
-    # TODO
+    arr = map(int, "-77 3 6 66 5".split()) #map(int, input().split())
+
+    maxx = -1000
+    runnerup = -1000
+    for i in arr:
+        if i > maxx:
+            runnerup = maxx
+            maxx = i
+        elif i < maxx and i > runnerup:
+            runnerup = i
+    print(runnerup)  
 
